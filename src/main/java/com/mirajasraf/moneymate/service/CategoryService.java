2eebb8ba203e979b2845d3cecb8c03f7d01b08d1
Miraj Asraf <mirajasraf786@gmail.com>
2026-01-17 16:22:15 +0530

    Category Entity, CategoryDTO, and Create Category Controller

---
+++ src/main/java/com/mirajasraf/moneymate/service/CategoryService.java
@@ -0,0 +1,48 @@
+package com.mirajasraf.moneymate.service;
+
+import com.mirajasraf.moneymate.dto.CategoryDTO;
+import com.mirajasraf.moneymate.entity.CategoryEntity;
+import com.mirajasraf.moneymate.entity.ProfileEntity;
+import com.mirajasraf.moneymate.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final ProfileService profileService;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryDTO saveCategory(CategoryDTO categoryDTO) {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        if (categoryRepository.existsByNameAndProfileId(categoryDTO.getName(), profile.getId())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Category with this name already exists");
+        }
+        CategoryEntity newCategory = toEntity(categoryDTO, profile);
+        newCategory = categoryRepository.save(newCategory);
+        return toDTO(newCategory);
+    }
+
+    private CategoryEntity toEntity(CategoryDTO categoryDTO, ProfileEntity profile) {
+        return CategoryEntity.builder()
+                .name(categoryDTO.getName())
+                .icon(categoryDTO.getIcon())
+                .profile(profile)
+                .type(categoryDTO.getType())
+                .build();
+    }
+
+    private CategoryDTO toDTO(CategoryEntity entity) {
+        return CategoryDTO.builder()
+                .id(entity.getId())
+                .profileId(entity.getProfile() != null ? entity.getProfile().getId() : null)
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .type(entity.getType())
+                .build();
+    }
+}
